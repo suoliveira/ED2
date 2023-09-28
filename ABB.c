7d4620370b6ec8ede758ef74b6f189b3d78dc6d7
Suellen Oliveira <107051108+suoliveira@users.noreply.github.com>
2023-09-28 16:30:58 -0300

    Add files via upload

--- ABB.c
+++ ABB.c
@@ -5,6 +5,7 @@ typedef struct No{
     int dado;
     struct No *esquerdo;
     struct No *direito;
+    int altura;
 } No;
 
 typedef struct{
@@ -12,20 +13,32 @@ typedef struct{
 } ABB;
 
 //Protótipo
-No* alocaMemoriaNo();
-ABB* alocaMemoriaABB();
-No* criaNo(int);
-ABB* criaABB();
-void removeABB(ABB*);
-No* buscaElementoABB(No*, int);
-No* removeElementoABB(No*, int);
-No* menorValor(No*);
-void liberaMemoriaABB(No*);
+No* alocaMemoriaNo(); 
+ABB* alocaMemoriaABB(); 
+No* criaNo(int); 
+ABB* criaABB(); 
+void removeABB(ABB*); 
+No* buscaElementoABB(No*, int); 
+No* removeElementoABB(No*, int); 
+No* menorValor(No*); 
+void liberaMemoriaABB(No*); 
 void escreveABB(ABB*);
 void insereNaABB(ABB*, int);
-No* inserir(No*, int);
+No* inserirDado(No*, int);
 void emOrdem(No*);
+void posOrdem(No*);
+void preOrdem(No*);
 
+
+//AVL
+int maior(int, int);
+int alturaDoNo(No*);
+int fatorDeBalanceamento(No*);
+No* ll(No*);
+No* rr(No*);
+No* lr(No*);
+No* rl(No*);
+No* balancear(No*);
 
 
 No* alocaMemoriaNo() {
@@ -43,6 +56,7 @@ No* criaNo(int dado){
         no->direito = NULL;
         no->esquerdo = NULL;
         no->dado = dado;
+        no->altura = 0;
     }
         
     return no;
@@ -65,18 +79,15 @@ void removeABB(ABB* abb){
 }
 
 No* buscaElementoABB(No* no, int dado) {
-    if (no != NULL && no->dado == dado) {
-        return no;
-    }
-    
     if (dado > no->dado){
-        no->direito = buscaElementoABB(no->direito, dado);
-    }else {
-        no->esquerdo = buscaElementoABB(no->esquerdo, dado);
+        buscaElementoABB(no->direito, dado);
+    }else if (dado < no->dado){
+        buscaElementoABB(no->esquerdo, dado);
+    }else{
+        printf("\nNodo com valor '%i' encontrado\n", no->dado);
     }
     
 }
-
 
 No* removeElementoABB(No* raiz, int dado) {
     if (raiz == NULL)
@@ -93,7 +104,6 @@ No* removeElementoABB(No* raiz, int dado) {
             return temp;
         } else if ( raiz->direito == NULL) {
             No* temp = raiz->esquerdo;
-            printf("\napagando: %i\n", raiz->dado);
             free(raiz);
             return temp;
         }
@@ -101,6 +111,9 @@ No* removeElementoABB(No* raiz, int dado) {
         raiz->dado = temp->dado;
         raiz->direito = removeElementoABB(raiz->direito, temp->dado);
     }
+
+    raiz->altura = maior(alturaDoNo(raiz->esquerdo), alturaDoNo(raiz->direito)) + 1;
+    raiz = balancear(raiz);
     return raiz;
 }
 
@@ -128,24 +141,28 @@ void escreveABB(ABB *abb){
 }
 
 void insereNaABB(ABB *abb, int valor){
-    if (abb != NULL)
-        abb->raiz = inserir(abb->raiz, valor);
+    if (abb != NULL){
+        abb->raiz = inserirDado(abb->raiz, valor);
+    }
 }
 
-
-No* inserir(No* no, int dado){
+No* inserirDado(No* no, int dado){
     if (no == NULL){
         return criaNo(dado);
     }
     
     if (dado > no->dado){
-        no->direito = inserir(no->direito, dado);
+        no->direito = inserirDado(no->direito, dado);
     }else {
-        no->esquerdo = inserir(no->esquerdo, dado);
+        no->esquerdo = inserirDado(no->esquerdo, dado);
     }
+
+    no->altura = maior(alturaDoNo(no->esquerdo), alturaDoNo(no->direito)) + 1;
+    no = balancear(no);
 
     return no;
 }
+
 void emOrdem(No* raiz) {
     if (raiz != NULL){
         emOrdem(raiz->esquerdo);
@@ -154,7 +171,99 @@ void emOrdem(No* raiz) {
     }
 }
 
+void posOrdem(No* raiz){
+    if (raiz != NULL){
+        posOrdem(raiz->esquerdo);
+        posOrdem(raiz->direito);
+        printf("%i, ", raiz->dado);
+    }
+}
 
+void preOrdem(No* raiz){
+    if(raiz != NULL){
+        printf("%i, ", raiz->dado);
+        preOrdem(raiz->esquerdo);
+        preOrdem(raiz->direito);
+    }
+}
+
+int maior(int a, int b){
+    if(a > b){
+        return a;
+    }else{
+        return b;
+    }
+}
+
+int alturaDoNo(No* no){
+    if (no == NULL){
+        return -1;
+    }else{
+        return no->altura;
+    } 
+}
+
+int fatorDeBalanceamento(No* no){
+    if (no != NULL){
+        return (alturaDoNo(no->esquerdo) - alturaDoNo(no->direito));
+    }else{
+        return 0;
+    }  
+}
+
+No* ll(No* raiz){
+    No* subArvore = raiz->direito;
+    No* filho = subArvore->esquerdo;
+
+    subArvore->esquerdo = raiz;
+    raiz->direito = filho;
+
+    raiz->altura = maior(alturaDoNo(raiz->esquerdo), alturaDoNo(raiz->direito)) + 1;
+    subArvore->altura = maior(alturaDoNo(subArvore->esquerdo), alturaDoNo(subArvore->direito)) + 1;
+    
+    return subArvore;
+ }
+
+ No* rr(No* raiz){
+    No* subArvore = raiz->esquerdo;
+    No* filho = subArvore->direito;
+
+    subArvore->direito = raiz;
+    raiz->esquerdo = filho;
+
+    raiz->altura = maior(alturaDoNo(raiz->esquerdo), alturaDoNo(raiz->direito)) + 1;
+    subArvore->altura = maior(alturaDoNo(subArvore->esquerdo), alturaDoNo(subArvore->direito)) + 1;
+    
+    return subArvore;
+ }
+
+No* lr(No* raiz){
+    raiz->esquerdo = ll(raiz->esquerdo);
+    return rr(raiz);
+}
+ 
+ 
+No* rl(No* raiz){
+    raiz->direito = rr(raiz->direito);
+    return ll(raiz);
+}
+
+No* balancear(No* raiz){
+    int fator = fatorDeBalanceamento(raiz);
+
+    if (fator < -1 && fatorDeBalanceamento(raiz->direito) <= 0){
+       raiz = ll(raiz);
+    }else if (fator > 1 && fatorDeBalanceamento(raiz->esquerdo) >= 0){
+        raiz = rr(raiz);
+    }else if (fator > 1 && fatorDeBalanceamento(raiz->esquerdo) < 0){
+        raiz = lr(raiz);
+    }else if (fator < -1 && fatorDeBalanceamento(raiz->direito) > 0){
+        raiz = rl(raiz);
+
+    }
+    
+    return raiz;
+}
 
 
 
@@ -162,20 +271,23 @@ int main(){
     ABB* abb;
     abb = criaABB();
 
+    insereNaABB(abb, 10);
     insereNaABB(abb, 7);
-    insereNaABB(abb, 8);
     insereNaABB(abb, 6);
+    insereNaABB(abb, 8);
     insereNaABB(abb, 12);
-    //insereNaABB(abb, 2);
-    //insereNaABB(abb, 42);
-    //insereNaABB(abb, 3);
-    //insereNaABB(abb, 20);
-    //insereNaABB(abb, 13);
-    //insereNaABB(abb, 4);
-    escreveABB(abb);
-    removeElementoABB(abb->raiz, 7);
-    //printf("\nEncontrado: %i \n", buscaElementoABB(abb->raiz, 12)->dado);
-    escreveABB(abb);
+    insereNaABB(abb, 11);
+    insereNaABB(abb, 13);
+    insereNaABB(abb, 20);
+    insereNaABB(abb, 15);
 
+    preOrdem(abb->raiz);
+    printf("\n");
+    emOrdem(abb->raiz);
+    printf("\n");
+    posOrdem(abb->raiz);
+
+    removeABB(abb);
     return 0;
+    
 }
